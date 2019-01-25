b939823ab2f268cfd19f38d779ccd420839733e6
Chris Deacy <deacy@uber.com>
2019-01-24 20:17:35 -0800

    updating file copyrights

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,4 +1,4 @@
-/** Copyright (c) 2017 Uber Technologies, Inc.
+/** Copyright (c) 2019 Uber Technologies, Inc.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
